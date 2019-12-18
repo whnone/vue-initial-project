448bd18090201134e3bb568b7d87e7c9e4af4a9a
haining.wang <haining.wang@d1m.cn>
2019-12-18 14:29:50 +0800

    U init组件

--- src/router/common-manage.js
+++ src/router/common-manage.js
@@ -2,17 +2,12 @@
  * 引入组件文件-配置路由
  */
 import Login from '@/pages/login'
-import HelloWorld from '@/components/HelloWorld'
 import Home from '../pages/home.vue'
 
 export default [{
   path: '/',
   component: Login,
   meta: { href: 'pages/login', title: '登录', name: 'Login' }
-}, {
-  path: '/HelloWorld',
-  component: HelloWorld,
-  meta: { href: 'components/HelloWorld', title: '测试', name: 'HelloWorld' }
 }, {
   path: '/pages/home',
   component: Home,
